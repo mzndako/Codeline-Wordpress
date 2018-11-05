2d0ee8a63f654a6608eb3e36b61e9e221bab1c33
unknown <mzndako@gmail.com>
2018-11-05 23:11:27 +0100

    Add the films post type to query

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -124,3 +124,14 @@ function be_register_taxonomies() {
 add_action( 'init', 'be_register_taxonomies' );
 
 
+
+// Add the film post type to query
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+ 
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() ){
+        $query->set( 'post_type', array( 'post', 'films' ) );
+	}
+    return $query;
+}
+
