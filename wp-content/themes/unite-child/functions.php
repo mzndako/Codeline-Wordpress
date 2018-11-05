e6d756dfbd64eefbe921d09711a4452e707dc858
unknown <mzndako@gmail.com>
2018-11-05 23:02:31 +0100

    Create the four taxonomy

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -62,3 +62,65 @@ function films_post_type() {
  
 add_action( 'init', 'films_post_type', 0 );
 
+
+function be_register_taxonomies() {
+	$taxonomies = array(
+		array(
+			'slug'         => 'genre',
+			'single_name'  => 'Genre',
+			'plural_name'  => 'Genre',
+			'post_type'    => 'films',
+			'rewrite'      => array( 'slug' => 'department' ),
+		),
+		array(
+			'slug'         => 'country',
+			'single_name'  => 'Country',
+			'plural_name'  => 'Countries',
+			'post_type'    => 'films'
+		),
+		array(
+			'slug'         => 'year',
+			'single_name'  => 'Year',
+			'plural_name'  => 'Years',
+			'post_type'    => 'films',
+		),
+		array(
+			'slug'         => 'actor',
+			'single_name'  => 'Actor',
+			'plural_name'  => 'Actors',
+			'post_type'    => 'films',
+		),
+	);
+	foreach( $taxonomies as $taxonomy ) {
+		$labels = array(
+			'name' => $taxonomy['plural_name'],
+			'singular_name' => $taxonomy['single_name'],
+			'search_items' =>  'Search ' . $taxonomy['plural_name'],
+			'all_items' => 'All ' . $taxonomy['plural_name'],
+			'parent_item' => 'Parent ' . $taxonomy['single_name'],
+			'parent_item_colon' => 'Parent ' . $taxonomy['single_name'] . ':',
+			'edit_item' => 'Edit ' . $taxonomy['single_name'],
+			'update_item' => 'Update ' . $taxonomy['single_name'],
+			'add_new_item' => 'Add New ' . $taxonomy['single_name'],
+			'new_item_name' => 'New ' . $taxonomy['single_name'] . ' Name',
+			'menu_name' => $taxonomy['plural_name']
+		);
+		
+		$rewrite = isset( $taxonomy['rewrite'] ) ? $taxonomy['rewrite'] : array( 'slug' => $taxonomy['slug'] );
+		$hierarchical = isset( $taxonomy['hierarchical'] ) ? $taxonomy['hierarchical'] : true;
+	
+		register_taxonomy( $taxonomy['slug'], $taxonomy['post_type'], array(
+			'hierarchical' => $hierarchical,
+			'labels' => $labels,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => $rewrite,
+		));
+	}
+	
+}
+
+// Register all the four taxonomies
+add_action( 'init', 'be_register_taxonomies' );
+
+
